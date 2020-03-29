2c27d59173204cf9ed1efe6a6cca9ebbe32fe292
shinnosuke-K <shin.050195@gmail.com>
2020-03-29 12:10:10 +0900

    add process
    GCP Cloud vision API を使用するための関数の記述

--- server.go
+++ server.go
@@ -1,9 +1,13 @@
 package HalalBot_GCP
 
 import (
+	"context"
+	"io"
 	"log"
 	"net/http"
 	"os"
+
+	vision "cloud.google.com/go/vision/apiv1"
 
 	"github.com/line/line-bot-sdk-go/linebot"
 )
@@ -23,6 +27,30 @@ func init() {
 	}
 }
 
+func ocr(ctn io.ReadCloser) string {
+	ctx := context.Background()
+
+	client, err := vision.NewImageAnnotatorClient(ctx)
+	if err != nil {
+		log.Println(err)
+		return "Not Annotate Client"
+	}
+
+	img, err := vision.NewImageFromReader(ctn)
+	if err != nil {
+		log.Println(err)
+		return "Not Image From Reader"
+	}
+
+	texts, err := client.DetectTexts(ctx, img, nil, 1)
+	if err != nil {
+		log.Println(err)
+		return "Not Detect Texts"
+	}
+
+	return texts[0].GetDescription()
+}
+
 func HalalBot(w http.ResponseWriter, r *http.Request) {
 	events, err := bot.ParseRequest(r)
 	if err != nil {
@@ -40,13 +68,15 @@ func HalalBot(w http.ResponseWriter, r *http.Request) {
 				if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(message.Text)).Do(); err != nil {
 					log.Println(err)
 				}
+
 			case *linebot.ImageMessage:
-				img, err := bot.GetMessageContent(message.ID).Do()
+				ctn, err := bot.GetMessageContent(message.ID).Do()
 				if err != nil {
 					log.Println(err)
 				}
-				defer img.Content.Close()
-				if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewImageMessage(message.OriginalContentURL, message.PreviewImageURL)).Do(); err != nil {
+
+				defer ctn.Content.Close()
+				if _, err := bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(ocr(ctn.Content))).Do(); err != nil {
 					log.Println(err)
 				}
 			}
